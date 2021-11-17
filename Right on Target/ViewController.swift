3ca0138d23345b4eea0a440edd6b2dbd253cb09c
Sergeev David Evgenievich <hwax0r@gmail.com>
2021-11-17 21:56:58 +0200

    Variables for slider and label created

--- Right on Target/ViewController.swift
+++ Right on Target/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()

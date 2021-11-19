555342794fd2f5823985e161fdfe5e2b678b2d65
Sergeev David Evgenievich <hwax0r@gmail.com>
2021-11-19 21:57:14 +0200

    UIViewController lifecycle	for ViewController completed

--- Right on Target/ViewController.swift
+++ Right on Target/ViewController.swift
@@ -47,6 +47,25 @@ class ViewController: UIViewController {
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
     
     @IBAction func checkNumber() {
         // получаем чило, на которое установлен ползунок слайдера

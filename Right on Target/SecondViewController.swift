be22984b9f0a53214b92b8f30c5aa04f75f39be8
Sergeev David Evgenievich <hwax0r@gmail.com>
2021-11-20 19:09:41 +0200

    IBAction hideCurrentScene created for the second view

--- Right on Target/SecondViewController.swift
+++ Right on Target/SecondViewController.swift
@@ -16,7 +16,7 @@ class SecondViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("loadView SecondViewController")
+        print("viewDidLoad SecondViewController")
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -39,14 +39,8 @@ class SecondViewController: UIViewController {
         print("viewDidDisappear SecondViewController")
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func hideCurrentScene() {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
 
 }

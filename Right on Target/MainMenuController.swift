b2baef247ad5f826df679fe80d6cdb9e88fdc2e6
Sergeev David Evgenievich <hwax0r@gmail.com>
2021-11-27 00:04:41 +0300

    ViewController for MainMenu created

---
+++ Right on Target/MainMenuController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Right on Target
+//
+//  Created by David Sergeev on 27.11.2021.
+//
+
+import UIKit
+
+class MainMenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}

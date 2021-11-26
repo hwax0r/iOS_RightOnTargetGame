1d9be04e8ea35c5c26c13e7cec9971e83fa1f5b6
Sergeev David Evgenievich <hwax0r@gmail.com>
2021-11-27 00:06:12 +0300

    ViewController for Colors Game created

---
+++ Right on Target/ColorsGameController.swift
@@ -0,0 +1,18 @@
+//
+//  ColorsGameController.swift
+//  Right on Target
+//
+//  Created by David Sergeev on 27.11.2021.
+//
+
+import UIKit
+
+class ColorsGameController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+}

cc32dd6ebc8d13629da2cff2f31c8818de43ee1b
Sergeev David Evgenievich <hwax0r@gmail.com>
2021-11-29 03:30:16 +0300

    Testing "Colors" game class on UI

--- Right on Target/ColorsGameController.swift
+++ Right on Target/ColorsGameController.swift
@@ -8,10 +8,47 @@
 import UIKit
 
 class ColorsGameController: UIViewController {
-
+    // Экземпляр игры
+    var game: colorsGame!
+    
+    // Метка с HEX значением текущего загаданного цвета
+    @IBOutlet var hexLabel: UILabel!
+    
+    // Кнопки слева направо
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    @IBOutlet var button4: UIButton!
+    // Нажатая кнопка
+    var pressedButton: UIButton?
+    
+    @IBAction func didPressButton(_ sender: UIButton){
+        print("Button: \(sender.title(for: .normal))")
+        pressedButton = sender
+        
+        if pressedButton != nil {
+            print("Button pressed")
+            if game.isCorrectAnswer(button: pressedButton!) {
+                print("OK")
+                game.newRound()
+                view.backgroundColor = game.secretColor
+            } else {
+                print("NO")
+            }
+            print()
+        }
+    }
+    
+    override func loadView() {
+        super.loadView()
+        game = colorsGame(buttons: [button1, button2, button3, button4],
+                          secretLabel: hexLabel)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        game.newRound()
+        view.backgroundColor = game.secretColor
     }
 
     

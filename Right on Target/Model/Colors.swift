4010e0a9b882421eeeaa4b7b571cfe12ecf0d0a8
Sergeev David Evgenievich <hwax0r@gmail.com>
2021-12-01 00:45:38 +0300

    The class has been rewritten to make a difference in business logic with '"Number guesser" game

--- Right on Target/Model/Colors.swift
+++ Right on Target/Model/Colors.swift
@@ -8,72 +8,119 @@
 import Foundation
 import UIKit
 
-protocol ColorsGameProtocol {
-    // Генератор цветов
-    var colorGenerator: HexColorGenerator { get }
-    // Массив кнопок
-    var buttons: [UIButton] { get }
-    // Загаданый цвет
-    var secretColor: UIColor { get }
-    var secretLabel: UILabel { get }
+protocol ColorGameProtocol {
+    // Количество заработанных очков
+    var score: Int { get }
+    //
+    var rounds: Int { get }
+    // Генератор случайных значений
+    var secretValueGenerator: HexColorGenerator { get }
+    // Загаданное значение
+    var secretValue: UIColor! { get }
     
-    // Получает вывод генератора и преобразует его в Hex-значение цвета
-    func colorIntToHexString(randomColor color: Int) -> String
-    // Получает вывод генератора и преобразует его в UIColor
-    func colorRGBToUIColor(rNorm: CGFloat, gNorm: CGFloat, bNorm: CGFloat) -> UIColor
-    // Сравнивает загаданный цвет с выбранным на кнопке
-    func isCorrectAnswer(button: UIButton) -> Bool
-    
-    init?(buttons: [UIButton], secretLabel: UILabel)
+    // Начало нового раунда
+    func newRound()
+    // Подсчитывает очки и начинает новый раунд
+    func compareAnswer(with: UIButton)
 }
 
-class colorsGame: ColorsGameProtocol {
-    var colorGenerator = HexColorGenerator()
-    var buttons: [UIButton] = []
-    var secretLabel = UILabel()
-    var secretColor = UIColor()
+class colorGame: ColorGameProtocol {
+    var score: Int = 0
+    var rounds: Int = 0
     
-    required init?(buttons: [UIButton], secretLabel: UILabel) {
-        guard buttons != [] else {
+    internal var secretValueGenerator = HexColorGenerator()
+    var secretValue: UIColor!
+    internal var buttonColors: [UIColor] = []
+    internal var buttons: [UIButton] = []
+    
+    init(from buttons: [UIButton]) {
+        self.buttons = buttons
+        newRound()
+    }
+    
+    func newRound() {
+        buttonColors = []
+        generateRandomColors()
+        selectSecretValue()
+    }
+    
+    func compareAnswer(with button: UIButton) {
+        rounds += 1
+        if button.title(for: .normal) == secretValue.hexString {
+            score += 1
+        } else {
+            guard score >= 1 else {
+                score = 0
+                return
+            }
+            score -= 1
+        }
+    }
+    
+    private func generateRandomColors(){
+        for button in buttons {
+            let newSecretValue: Int = secretValueGenerator.getRandomValue()
+            let normColors = secretValueGenerator.getRGBNormed()
+            let buttonColor: UIColor = colorRGBToUIColor(rNorm: normColors!.0,
+                                                         gNorm: normColors!.1,
+                                                         bNorm: normColors!.2)
+            let buttonText: String = colorIntToHexString(randomColor: newSecretValue)
+            
+            button.setTitle(buttonText, for: .normal)
+            buttonColors.append(buttonColor)
+        }
+    }
+    
+    private func selectSecretValue(){
+        let randomIndex: Int = (0..<buttonColors.count).randomElement()!
+        secretValue = buttonColors[randomIndex]
+    }
+    
+    private func colorRGBToUIColor(rNorm: CGFloat, gNorm: CGFloat, bNorm: CGFloat) -> UIColor {
+            let color = UIColor(red: rNorm,
+                                green: gNorm,
+                                blue: bNorm,
+                                alpha: 1)
+            return color
+        }
+    
+    private func colorIntToHexString(randomColor color: Int) -> String {
+            var hexString = NSString(format:"#%06x", color) as String
+            hexString = hexString.uppercased()
+            return hexString
+        }
+}
+
+class HexColorGenerator: GeneratorProtocol {
+    /*
+     Class generates HEX color of format #000000...FFFFFF
+     */
+    
+    private var rNorm: CGFloat? = nil
+    private var gNorm: CGFloat? = nil
+    private var bNorm: CGFloat? = nil
+    
+    init() {}
+    
+    func getRandomValue() -> Int {
+        let red   = (0...255).randomElement()!
+        let green = (0...255).randomElement()!
+        let blue  = (0...255).randomElement()!
+        
+        rNorm = CGFloat(red)/255
+        gNorm = CGFloat(green)/255
+        bNorm = CGFloat(blue)/255
+        
+        let hexColor: Int = red<<16 | green<<8 | blue<<0
+        
+        return hexColor
+    }
+    
+    func getRGBNormed() -> (CGFloat, CGFloat, CGFloat)? {
+        guard rNorm != nil else {
             return nil
         }
-        self.buttons = buttons
-        self.secretLabel = secretLabel
-    }
-    
-    func colorIntToHexString(randomColor color: Int) -> String {
-        var hexString = NSString(format:"#%06x", color) as String
-        hexString = hexString.uppercased()
-        return hexString
-    }
-    
-    func colorRGBToUIColor(rNorm: CGFloat, gNorm: CGFloat, bNorm: CGFloat) -> UIColor {
-        let color = UIColor(red: rNorm,
-                            green: gNorm,
-                            blue: bNorm,
-                            alpha: 1)
-        return color
-    }
-    
-    func isCorrectAnswer(button: UIButton) -> Bool{
-        secretColor == button.backgroundColor
-    }
-    
-    
-    
-    func newRound(){
-        for button in buttons {
-            let color: Int = colorGenerator.getRandomValue()
-            let colorName: String = self.colorIntToHexString(randomColor: color)
-            button.backgroundColor = self.colorRGBToUIColor(rNorm: colorGenerator.rNorm,
-                                                            gNorm: colorGenerator.gNorm,
-                                                            bNorm: colorGenerator.bNorm)
-            button.setTitle(colorName, for: .normal)
-        }
-        
-        let randomIndex: Int = (0...3).randomElement()!
-        secretColor = buttons[randomIndex].backgroundColor!
-        secretLabel.text = buttons[randomIndex].backgroundColor!.hexString
+        return (rNorm!, gNorm!, bNorm!)
     }
 }
 
